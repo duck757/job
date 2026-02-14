88d6e4caf126431458dde960f173113482a8c9a0
duck757 <maverickwillson4@gmail.com>
2026-02-14 07:16:12 +0530

    Increase random wait time range from 1-100 to 1-300

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ async function startRandomCountingLoop(client, CHANNEL_ID) {
   const channel = await client.channels.fetch(CHANNEL_ID);
 
   while (true) {
-    const waitTime = randInt(1, 100) * 60 * 1000;
+    const waitTime = randInt(1, 300) * 60 * 1000;
     logStatus(client, "Sleeping", `Waiting ${Math.floor(waitTime / 60000)} mins`);
     await sleep(waitTime);
 
